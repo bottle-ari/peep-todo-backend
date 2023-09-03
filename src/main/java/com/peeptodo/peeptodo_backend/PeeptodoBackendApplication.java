12a31447cd85e125612e157018dac222ec3e8cb5
Haneol <haneolj@gmail.com>
2023-09-03 15:00:30 +0900

    [FEAT] Add GAE settings

--- src/main/java/com/peeptodo/peeptodo_backend/PeeptodoBackendApplication.java
+++ src/main/java/com/peeptodo/peeptodo_backend/PeeptodoBackendApplication.java
@@ -2,8 +2,9 @@ package com.peeptodo.peeptodo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) // 스프링 시큐리티 일단 꺼두었음, 나중에 추가
 public class PeeptodoBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(PeeptodoBackendApplication.class, args);
