af4da6efae680cdad74bf61e6095792b0d215007
Haneol <haneolj@gmail.com>
2023-09-05 22:14:02 +0900

    [FEAT] Implement OAuth2 redirection for test(2)

--- src/main/java/com/peeptodo/peeptodo_backend/controller/OAuth2Controller.java
+++ src/main/java/com/peeptodo/peeptodo_backend/controller/OAuth2Controller.java
@@ -1,6 +1,6 @@
 package com.peeptodo.peeptodo_backend.controller;
 
-import com.peeptodo.peeptodo_backend.service.GoogleOAuthUserService;
+import com.peeptodo.peeptodo_backend.service.GoogleOAuth2Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuth2Controller {
 
     @Autowired
-    private GoogleOAuthUserService googleOAuthUserService;  // 사용자와 JWT 처리 서비스
+    private GoogleOAuth2Service googleOAuth2Service;
 
     @GetMapping("/api/oauth2/callback/google")
     public ResponseEntity<?> successGoogleLogin(@RequestParam String code) {
-        return ResponseEntity.ok().body(null);
+        return googleOAuth2Service.getGoogleAccessToken(code);
     }
 }
 
