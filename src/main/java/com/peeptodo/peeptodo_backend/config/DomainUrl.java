87619fcb6da9d9f567beb80eb8d6daeaadd129d8
Haneol <haneolj@gmail.com>
2023-09-13 16:49:13 +0900

    [FIX] Update DomainUrl.java

--- src/main/java/com/peeptodo/peeptodo_backend/config/DomainUrl.java
+++ src/main/java/com/peeptodo/peeptodo_backend/config/DomainUrl.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 @Getter
 public enum DomainUrl {
 //    // local test
-//    FRONTEND("http://localhost:3000");
+//    FRONTEND("http://localhost:3000"),
     // infra
-    FRONTEND("https://peeptodo.com");
+    FRONTEND("https://peeptodo.com"),
 
-    BACKEND("http://localhost:8080"),
+    BACKEND("http://localhost:8080");
     
     private final String value;
 
